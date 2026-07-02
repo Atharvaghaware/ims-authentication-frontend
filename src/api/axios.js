e94855adc1417fcf22e6426e4ad12ad55c6ca1bf
Atharvaghaware <atharvaghaware0007@gmail.com>
2026-07-02 16:15:40 +0530

    git api file missing as been updated

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,3 +1,10 @@
+import axios from "axios";
+
 const api = axios.create({
-  baseURL: "https://ims-authentication-backend-2.onrender.com/api",
+  baseURL: import.meta.env.VITE_API_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
+
+export default api;
